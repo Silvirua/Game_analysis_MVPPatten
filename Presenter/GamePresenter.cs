d9e8934899eaf299330120dfcd533e214b6fb0f6
Silvirua <silvirua@naver.com>
2026-04-09 16:18:49 +0900

    Update GamePresenter.cs

--- Presenter/GamePresenter.cs
+++ Presenter/GamePresenter.cs
@@ -1,6 +1,23 @@
 using Game_analysis_MVPPattern.Model;
 
 namespace Game_analysis_MVPPattern.Presenter{
-  
+  public class GamePresenter{
+    private IGameView _view;
+    private IGameLogic _model;
+
+    public GamePresenter(IGameView view, IGameLogic model){
+      _view = view;
+      _model = model;
+      Init();
+    }
+
+    private void Init(){
+      _view.OnInputReceived += HandlePlayerInput;
+
+      _model.OnDrawResult += log => _view.PrintLog(log);
+    }
+
+    private void HandlePlayerInput(int inputCode){
+      _model.Process
 
 }
