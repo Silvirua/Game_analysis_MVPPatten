432d39d12e3d977e49c1a8533668613c7bc5d43b
Silvirua <silvirua@naver.com>
2026-04-09 16:01:04 +0900

    Update IGameView.cs

--- Presenter/IGameView.cs
+++ Presenter/IGameView.cs
@@ -1,3 +1,14 @@
 namespace Game_analysis_MVPPattern.Presenter{
+  public interface IGameView{
+    event Action<int> OnInputReceived;
 
+    // Player가 가지고 있는 재화 갱신
+    void UpdatePlayerGems(int gem); 
+
+    // Log 표시
+    void PrintLog(string message);
+
+    // 게임 프로세스
+    void StartGameLoop();
+  }
 }
