db818d32e938569b312ac4fe84250990574364c5
Silvirua <silvirua@naver.com>
2026-04-10 18:28:44 +0900

    Update GamePresenter.cs
    코드 추가 및 수정

--- Presenter/GamePresenter.cs
+++ Presenter/GamePresenter.cs
@@ -1,23 +1,36 @@
-using Game_analysis_MVPPattern.Model;
+using MVPPattern.Model;
 
-namespace Game_analysis_MVPPattern.Presenter{
-  public class GamePresenter{
-    private IGameView _view;
-    private IGameLogic _model;
+namespace Game_analysis_MVPPattern.Presenter
+{
+    internal class GamePresenter
+    {
+        private IGameView _view;
+        private IGameLogic _model;
 
-    public GamePresenter(IGameView view, IGameLogic model){
-      _view = view;
-      _model = model;
-      Init();
+        public GamePresenter(IGameView view, IGameLogic model)
+        {
+            _view = view;
+            _model = model;
+            Init();
+        }
+
+        private void Init()
+        {
+            _view.UpdatePlayerFGems(_model.Fgem);
+            _view.UpdatePlayerPGems(_model.Pgem);
+
+            _view.OnInputReceived += HandlePlayerInput;
+
+            _model.OnPlayerPgemChanged += (amount) => _view.UpdatePlayerPGems(amount);
+            _model.OnPlayerFgemChanged += (amount) => _view.UpdatePlayerFGems(amount);
+
+            _model.OnDrawResultLog += (message) => _view.PrintLog(message);
+            _model.OnGameExit += (isExit) => { if (isExit) _view.ShowGameExit(); };
+        }
+
+        private void HandlePlayerInput(int inputCode)
+        {
+            _model.DrawProcess((ActionType)inputCode);
+        }
     }
-
-    private void Init(){
-      _view.OnInputReceived += HandlePlayerInput;
-
-      _model.OnDrawResult += log => _view.PrintLog(log);
-    }
-
-    private void HandlePlayerInput(int inputCode){
-      _model.Process
-
 }
