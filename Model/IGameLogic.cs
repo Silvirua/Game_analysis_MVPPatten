a196142c6486be797a80ea47793165589040a485
Silvirua <silvirua@naver.com>
2026-04-10 18:17:05 +0900

    Update IGameLogic.cs

--- Model/IGameLogic.cs
+++ Model/IGameLogic.cs
@@ -1,11 +1,19 @@
 namespace Game_analysis_MVPPattern.Model{
-  public interface IGameLogic{
-    // player 재화 변경 이벤트
-    public event Action<int> OnPlayerGemChanged;
+    internal interface IGameLogic{
+        int Fgem{get;}
+        int Pgem{get;}
 
-    // 가챠 결과 이벤트
-    public event Action<string> OnDrawResultLog;
+        // 플레이어 무료 재화 변경 이벤트
+        public event Action<int> OnPlayerFgemChanged;
+        // 플레이어 유료 재화 변경 이벤트
+        public event Action<int> OnPlayerPgemChanged;
 
-    // 
+        // 게임 종료 상태 발생 이벤트
+        public event Action<bool> OnGameExit;
 
+        // 플레이어 봅기 결과 메세지 발생 이벤트
+        public event Action<string> OnDrawResultLog;
+
+        // 뽑기 프로세스
+        public void DrawProcess(ActionType playerAction);
 }
